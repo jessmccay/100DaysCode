acb780be93a408897c35f7aff7b52239f8a67fca
jessmccay <jessmccay@gmail.com>
2017-04-11 23:24:00 -0700

    more javascript switch cases, integrated with if statements

--- javascript.js
+++ javascript.js
@@ -568,3 +568,116 @@ function abTest(a, b) {
 
 // Change values below to test your code
 abTest(2,2);
+
+// Counting Cards
+// In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+//
+// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+//
+// Count Change	Cards
+// +1	2, 3, 4, 5, 6
+// 0	7, 8, 9
+// -1	10, 'J', 'Q', 'K', 'A'
+// You will write a card counting function. It will receive a card parameter and increment or decrement the global count variable according to the card's value (see table). The function will then return a string with the current count and the string "Bet" if the count is positive, or "Hold" if the count is zero or negative. The current count and the player's decision ("Bet" or "Hold") should be separated by a single space.
+//
+// Example Output
+// "-3 Hold"
+// "5 Bet"
+//
+// Hint
+// Do NOT reset count to 0 when value is 7, 8, or 9.
+var count = 0;
+
+function cc(card) {
+  // Only change code below this line
+
+  switch(card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+     count = count +1;
+     break;
+    case 7:
+    case 8:
+    case 9:
+      count = count;
+      break;
+    case 10:
+    case 'J':
+    case 'Q':
+    case 'K':
+    case 'A':
+      count = count -1;
+
+  }
+  if(count > 0){
+    return count + " Bet";
+  }
+  return count + " Hold";
+
+  // Only change code above this line
+}
+
+// Add/remove calls to test your function.
+// Note: Only the last will display
+cc(2); cc(3); cc(7); cc('K'); cc('A');
+
+
+// Instructions
+// Make an object that represents a dog called myDog which contains the properties "name" (a string), "legs", "tails" and "friends".
+
+// You can set these object properties to whatever values you want, as long "name" is a string, "legs" and "tails" are numbers, and "friends" is an array.
+
+// Example
+var ourDog = {
+  "name": "Camper",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["everything!"]
+};
+
+// Only change code below this line.
+
+var myDog = {
+  "name": "Rolo",
+  "legs": 4,
+  "tails": 1,
+  "friends": ["Me", "Nereus", "Jayden", "Jordan"]
+
+
+
+
+};
+
+
+// Instructions
+// Read in the property values of testObj using dot notation. Set the variable hatValue equal to the object property hat and set the variable shirtValue equal to the object property shirt.
+
+// Setup
+var testObj = {
+  "hat": "ballcap",
+  "shirt": "jersey",
+  "shoes": "cleats"
+};
+
+// Only change code below this line
+
+var hatValue = testObj.hat;      // Change this line
+var shirtValue = testObj.shirt;    // Change this line
+
+// Instructions
+// Read the values of the properties "an entree" and "the drink" of testObj using bracket notation and assign them to entreeValue and drinkValue respectively.
+
+// Setup
+var testObj = {
+  "an entree": "hamburger",
+  "my side": "veggies",
+  "the drink": "water"
+};
+
+// Only change code below this line
+
+var entreeValue = testObj["an entree"];   // Change this line
+var drinkValue = testObj["the drink"];    // Change this line
