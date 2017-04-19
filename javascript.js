5587d2a6597785fa53766f2314a1e08508f36f56
jessmccay <jessmccay@gmail.com>
2017-04-18 22:03:10 -0700

    nested forloops, Math.random, Math.floor

--- javascript.js
+++ javascript.js
@@ -990,3 +990,123 @@ while(i<5) {
   myArray.push(i);
   i++;
 }
+
+// Day 14 >>
+// Instructions
+// Modify function multiplyAll so that it multiplies the product variable by each number in the sub-arrays of arr
+function multiplyAll(arr) {
+  var product = 1;
+  // Only change code below this line
+  for(var i = 0; i < arr.length; i++){
+    for(var j =0; j < arr[i].length; j ++)
+    product *= arr[i][j];
+  }
+  // Only change code above this line
+  return product;
+}
+
+// Modify values below to test your code
+multiplyAll([[1,2],[3,4],[5,6,7]]);
+
+// Profile Lookup
+// We have an array of objects representing different people in our contacts lists.
+//
+// A lookUpProfile function that takes firstName and a property (prop) as arguments has been pre-written for you.
+//
+// The function should check if firstName is an actual contact's firstName and the given property (prop) is a property of that contact.
+//
+// If both are true, then return the "value" of that property.
+
+// If firstName does not correspond to any contacts then return "No such contact"
+
+// If prop does not correspond to any valid properties then return "No such property"
+//Setup
+var contacts = [
+    {
+        "firstName": "Akira",
+        "lastName": "Laine",
+        "number": "0543236543",
+        "likes": ["Pizza", "Coding", "Brownie Points"]
+    },
+    {
+        "firstName": "Harry",
+        "lastName": "Potter",
+        "number": "0994372684",
+        "likes": ["Hogwarts", "Magic", "Hagrid"]
+    },
+    {
+        "firstName": "Sherlock",
+        "lastName": "Holmes",
+        "number": "0487345643",
+        "likes": ["Intriguing Cases", "Violin"]
+    },
+    {
+        "firstName": "Kristian",
+        "lastName": "Vos",
+        "number": "unknown",
+        "likes": ["Javascript", "Gaming", "Foxes"]
+    }
+];
+
+
+function lookUpProfile(firstName, prop){
+// Only change code below this line
+  for (var i=0;i<contacts.length;i++) {
+        if (contacts[i].firstName===firstName) {
+            if (contacts[i][prop]) {
+                return contacts[i][prop];
+            }
+            else {
+                return "No such property";
+            }
+        }
+    }
+  return "No such contact";
+// Only change code above this line
+}
+
+// Change these values to test your function
+lookUpProfile("Akira", "likes");
+
+// Instructions
+// Change randomFraction to return a random number instead of returning 0.
+function randomFraction() {
+
+  // Only change code below this line.
+  var ranNum = Math.random();
+  return ranNum;
+
+  // Only change code above this line.
+}
+
+// Instructions
+// Use this technique to generate and return a random whole number between 0 and 9.
+var randomNumberBetween0and19 = Math.floor(Math.random() * 20);
+
+function randomWholeNum() {
+
+  // Only change code below this line.
+
+  return Math.floor(Math.random() * 10);
+}
+
+// Instructions
+// Create a function called randomRange that takes a range myMin and myMax and returns a random number that's greater than or equal to myMin, and is less than or equal to myMax, inclusive.
+// Example
+function ourRandomRange(ourMin, ourMax) {
+
+  return Math.floor(Math.random() * (ourMax - ourMin + 1)) + ourMin;
+}
+
+ourRandomRange(1, 9);
+
+// Only change code below this line.
+
+function randomRange(myMin, myMax) {
+
+  return Math.floor(Math.random() *(myMax - myMin + 1)) + myMin; // Change this line
+
+}
+
+// Change these values to test your function
+var myRandom = randomRange(5, 15);
