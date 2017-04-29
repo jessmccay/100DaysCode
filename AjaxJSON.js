14d1b4fd3611ff621afeaac67e2b3b5ffb4bb4b5
jessmccay <jessmccay@gmail.com>
2017-04-28 22:39:15 -0700

    finished ajax

--- AjaxJSON.js
+++ AjaxJSON.js
@@ -17,7 +17,9 @@
   $(document).ready(function() {
     // Only change code below this line.
     $("#getMessage").on("click", function(){
-
+      $.getJSON("/json/cats.json", function(json) {
+$(".message").html(JSON.stringify(json));
+});
 });
     // Only change code obove this line.
   });
@@ -79,4 +81,121 @@
       </button>
     </div>
   </div>
+</div>
+
+// Render Images from Data Sources
+// We've seen from the last two lessons that each object in our JSON array contains an imageLink key with a value that is the URL of a cat's image.
+//
+// When we're looping through these objects, let's use this imageLink property to display this image in an img element.
+"alt='" + val.altText + "'>";
+
+
+          // Only change code above this line.
+
+          html += "</div>";
+
+        });
+
+        $(".message").html(html);
+
+      });
+    });
+  });
+</script>
+
+<div class="container-fluid">
+  <div class = "row text-center">
+    <h2>Cat Photo Finder</h2>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12 well message">
+      The message will go here
+    </div>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12">
+      <button id = "getMessage" class = "btn btn-primary">
+        Get Message
+      </button>
+    </div>
+  </div>
+</div>
+
+// // Prefilter JSON
+// If we don't want to render every cat photo we get from our Free Code Camp's Cat Photo JSON API, we can pre-filter the json before we loop through it.
+//
+// Let's filter out the cat whose "id" key has a value of 1.
+<script>
+  $(document).ready(function() {
+
+    $("#getMessage").on("click", function() {
+      $.getJSON("/json/cats.json", function(json) {
+
+        var html = "";
+
+        // Only change code below this line.
+        json = json.filter(function(val) {
+  return (val.id !== 1);
+});
+
+
+        // Only change code above this line.
+
+        json.forEach(function(val) {
+
+          html += "<div class = 'cat'>"
+
+          html += "<img src = '" + val.imageLink + "' " + "alt='" + val.altText + "'>"
+
+          html += "</div>"
+
+        });
+
+        $(".message").html(html);
+
+      });
+    });
+  });
+</script>
+
+<div class="container-fluid">
+  <div class = "row text-center">
+    <h2>Cat Photo Finder</h2>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12 well message">
+      The message will go here
+    </div>
+  </div>
+  <div class = "row text-center">
+    <div class = "col-xs-12">
+      <button id = "getMessage" class = "btn btn-primary">
+        Get Message
+      </button>
+    </div>
+  </div>
+</div>
+
+// Get Geolocation Data
+// Another cool thing we can do is access our user's current location. Every browser has a built in navigator that can give us this information.
+//
+// The navigator will get our user's current longitude and latitude.
+//
+// You will see a prompt to allow or block this site from knowing your current location. The challenge can be completed either way, as long as the code is correct.
+//
+// By selecting allow you will see the text on the output phone change to your latitude and longitude
+<script>
+  // Only change code below this line.
+
+  if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(function(position) {
+    $("#data").html("latitude: " + position.coords.latitude + "<br>longitude: " + position.coords.longitude);
+  });
+}
+
+  // Only change code above this line.
+</script>
+<div id = "data">
+  <h4>You are here:</h4>
+
 </div>
