c005509b16479c44016e8e74b1e0cc9997bc0a10
jessmccay <jessmccay@gmail.com>
2017-07-10 08:50:12 -0700

    queue and set data structures

--- javascript.js
+++ javascript.js
@@ -1873,3 +1873,56 @@ function rot13(str) { // LBH QVQ VG!
 
 // Change the inputs below to test
 rot13("SERR PBQR PNZC");
+
+
+
+
+//DATA STRUCTURES//
+//QUEUES//
+this.enqueue = function(element){
+    collection.push(element);
+  };
+
+  this.dequeue = function(){
+   return collection.shift();
+
+  };
+  this.front = function(){
+    return collection[0];
+  };
+  this.size = function(){
+    return collection.length;
+  };
+
+  this.isEmpty = function(){
+    if(collection.length === 0){
+      return true;
+    }
+  };
+
+
+ //SETS------------------------------------------//
+
+function Set() {
+    // the var collection will hold our set
+    var collection = [];
+    // this method will check for the presence of an element and return true or false
+    this.has = function(element) {
+        return (collection.indexOf(element) !== -1);
+    };
+    // this method will return all the values in the set
+    this.values = function() {
+        return collection;
+    };
+    // change code below this line
+    this.add = function(element){
+      if(!this.has(element)){
+        collection.push(element);
+        return true;
+      }
+      else{
+        return false;
+      }
+    };
+    // change code above this line
+}
